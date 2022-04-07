d07b9a3698b4ddba2b4fe8aef16c9508c7a7390a
lattesmoody <lattesmoody@gmail.com>
2022-04-08 00:28:30 +0900

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import { useState, useEffect } from "react";
 
 function Hello(){
+    // function byFn(){
+    //     console.log("bye :(");
+    // }
+    // function hiFn(){
+    //     console.log("created :)");
+    //     return byFn;
+    // }
+    useEffect(function (){
+        console.log("hi :)");
+        return () => console.log("bye :(");
+    },[]);
+        //return () => console.log("destroyed :(");
+        //윗줄: cleanup function => component가 destroy될 때
     return <h1>Hello</h1>;
 }
 
