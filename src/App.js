700ef101e067679edb94291170c6aa5aa556af0c
lattesmoody <lattesmoody@gmail.com>
2022-04-14 20:40:47 +0900

    2.4.2 배열 구조 분해

--- src/App.js
+++ src/App.js
@@ -1,44 +1,8 @@
-// const logCompliment = function(firstName,message){
-//   return `${firstName}: ${message}`;
-// };
-
-// console.log(logCompliment("펭귄토끼", "아주 멋져요"));
-// function logActivity(name="팽귄토끼", activity="힙합"){
-//   console.log(`${name}은 ${activity}를 좋아합니다.`);
-// }
-// logActivity();
-
-// const defaultPerson = {
-//   name: {
-//     first: "토끼",
-//     last: "펭귄"
-//   },
-//   favActivity: "힙합"
-// };
-// function logActivity(p=defaultPerson){
-//   console.log(`${p.name.first}은(는) ${p.favActivity}를 좋아합니다.`);
-// }
-// logActivity();
-const lordify = ({spouse:{firstname}}) => {
-  console.log(`켄터베리의 ${firstname}`)
-};
-
-const regularPerson={
-  firstname: "현석",
-  lastname: "오",
-  spouse: {
-    firstname:"펭귄!",
-    lastname: "토끼"
-  }
-};
-
-lordify(regularPerson);
+//const [firstAnimal] = ["캥거루", "웜뱃", "코알라"];
+const [,,thirdAnimal] = ["캥거루", "웜뱃", "코알라"];
+console.log(thirdAnimal);
 
 function App() {
   
 }
 export default App;
-
-//useEffect =>특정 코드의 실행을 제한한다.
-// => component가 처음 render할때 실행되고
-// 다시는 실행되지 않을 function을 넣는다.
