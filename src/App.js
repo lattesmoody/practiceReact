31a65f229b5d051104f250443d9e8f255c5cc33f
lattesmoody <lattesmoody@gmail.com>
2022-04-15 01:05:31 +0900

    2.5.2 async/await

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-// 1.
-// console.log(fetch("https://api.randomuser.me/?nat=US&results=1"));
+// then 함수: 연쇄 호출하여 프로미스의 결과를 기다림.
+// async 함수: 프로미스 다음에 있는 코드 실행하기 전에 프로미스 끝날 때까지 기다리라고 명령 가능
 
-// 2. 
-// fetch("https://api.randomuser.me/?nat=US&results=1").then (res=>
-//   console.log(res.json())
-// );
+// 아래 코드는 randomuser.me에 대한 요청. 
+const getFakePerson = async () => { // await은 getFakePerson을 비동기 함수로 만들어준다.
+  try{
+  let res = await fetch("https://api.randomuser.me/?nat=US&results=1");
+  let {results} = res.json();
+  console.log(results);
+  }catch(error){
+    console.error(error);
+  }
+};
 
-// 3.
-fetch("https://api.randomuser.me/?nat=US&results=1")
-  .then(res => res.json())
-  .then(json => json.results)
-  .then(console.log)
-  .catch(console.error);
+getFakePerson();
 
 function App() {
   
